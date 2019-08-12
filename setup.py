f8a7e730542955d03cd635fe9fe13b5aa0f9b6a5
Naresh-kumar-B-N <53941709+Naresh-kumar-B-N@users.noreply.github.com>
2019-08-12 20:12:19 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="druidpy",
-    version="1.0.1",
+    version="1.0.2",
     author="Naresh Kumar B N",
     author_email="nareshbn007@gmail.com",
     description="A package which provides minimum required methods for working with Druid through Python!",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Naresh-kumar-B-N/druidpy/archive/v1.0.1.tar.gz",
+    url="https://github.com/Naresh-kumar-B-N/druidpy/archive/v1.0.2.tar.gz",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
